7a8f8fd9d625eeb7433b090da7135238e289df90
PawanKalhara <pawanperera926@gmail.com>
2024-03-01 15:02:22 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,119 @@
+(function ($) {
+    "use strict";
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1000, 'easeInOutExpo');
+        return false;
+    });
+    
+    
+    // Dropdown on mouse hover
+    $(document).ready(function () {
+        function toggleNavbarMethod() {
+            if ($(window).width() > 992) {
+                $('.navbar .dropdown').on('mouseover', function () {
+                    $('.dropdown-toggle', this).trigger('click');
+                }).on('mouseout', function () {
+                    $('.dropdown-toggle', this).trigger('click').blur();
+                });
+            } else {
+                $('.navbar .dropdown').off('mouseover').off('mouseout');
+            }
+        }
+        toggleNavbarMethod();
+        $(window).resize(toggleNavbarMethod);
+    });
+
+
+    // Testimonials carousel
+    $(".testimonials-carousel").owlCarousel({
+        autoplay: true,
+        dots: true,
+        loop: true,
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
+    });
+    
+    
+    // Portfolio isotope and filter
+    var portfolioIsotope = $('.portfolio-container').isotope({
+        itemSelector: '.portfolio-item',
+        layoutMode: 'fitRows'
+    });
+
+    $('#portfolio-flters li').on('click', function () {
+        $("#portfolio-flters li").removeClass('filter-active');
+        $(this).addClass('filter-active');
+
+        portfolioIsotope.isotope({filter: $(this).data('filter')});
+    });
+    
+})(jQuery);
+
+window.addEventListener('scroll', function() {
+    const header = document.querySelector('.header');
+    const logo = document.querySelector('.header .brand img');
+    
+    // When the scroll is more than 50 pixels, add the "scrolled" class, otherwise remove it
+    if (window.scrollY > 50) {
+        header.classList.add('scrolled');
+        logo.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+        logo.classList.remove('scrolled');
+    }
+});
+
+
+const trustedLogosWrapper = document.querySelector('.trusted-logos-wrapper');
+trustedLogosWrapper.addEventListener('mouseover', () => {
+    trustedLogosWrapper.style.animationPlayState = 'paused';
+});
+
+trustedLogosWrapper.addEventListener('mouseout', () => {
+    trustedLogosWrapper.style.animationPlayState = 'running';
+});
+
+
+const allFaqs = Array.from(document.querySelectorAll('#accordion .card'));
+allFaqs.forEach((faq, index) => {
+    if (index >= 3) { // Hide all FAQs starting from the fourth one
+        faq.style.display = 'none';
+    }
+});
+
+// Function to toggle the rest of the FAQs
+function toggleMoreFaqs() {
+    const buttonText = document.getElementById('toggleMoreFaqs');
+    allFaqs.forEach((faq, index) => {
+        if (index >= 3) { // Only toggle display for FAQs after the third
+            if (faq.style.display === 'none') {
+                faq.style.display = ''; // Show FAQ
+                buttonText.textContent = 'Show Less'; // Update button text
+            } else {
+                faq.style.display = 'none'; // Hide FAQ
+                buttonText.textContent = 'Show More'; // Update button text
+            }
+        }
+    });
+}
